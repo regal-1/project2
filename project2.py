707af7de923d16f2a6944f6926b6905e88c54cdf
Rishika <rishika@Rishikas-MacBook-Pro-2.local>
2025-03-04 22:13:03 -0800

    working nearest neighbor function

--- project2.py
+++ project2.py
@@ -1,5 +1,6 @@
 #nearest neighbor algorithm
 import pandas as pd # type: ignore
+import numpy as np # type: ignore
 
 def read_data(fileName):
     
@@ -13,7 +14,19 @@ def read_data(fileName):
     df_class1 = df_class1.iloc[:, 1:]
     df_class2 = df_class2.iloc[:, 1:]
 
-    return df_class1, df_class2
+    #convert to 1d array
+    return df_class1.to_numpy().flatten(), df_class2.to_numpy().flatten()
+
+#compute distance between two points
+def distance(a, b):
+    return np.sqrt(((a - b) ** 2))
+
+#finds nearest neighbor in array
+def find_nearest(class1, data):
+    dist = np.array([distance(data, x) for x in class1])
+    nearest = np.argmin(dist)
+    return nearest
+
 
 def debug_csv():
     class1, class2 = read_data("CS170_Small_Data__10.txt")
@@ -21,5 +34,7 @@ def debug_csv():
     print(class1)
     print("\nClass 2:")
     print(class2)
+    index = (find_nearest(class1, 1.1))
+    print(index, class1[index])
 
 debug_csv()
