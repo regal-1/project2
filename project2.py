71495017a173f91ac17c470c1cb456891c6b96a9
Rishika <rishika@Rishikas-MacBook-Pro-2.local>
2025-03-07 23:11:17 -0800

    started feature search function + some pseudocode, fixed nearest neighbor alg

--- project2.py
+++ project2.py
@@ -2,31 +2,32 @@
 import pandas as pd # type: ignore
 import numpy as np # type: ignore
 
-def read_data(fileName):
-    
+def read_data(fileName):  
     #read file using pandas
     df = pd.read_csv(fileName, header=None, delim_whitespace=True)
 
-    # Filter rows by class label using boolean indexing
+    #filter rows by class label; boolean indexing
     df_class1 = df[df.iloc[:, 0].astype(int) == 1]
     df_class2 = df[df.iloc[:, 0].astype(int) == 2]
 
     df_class1 = df_class1.iloc[:, 1:]
     df_class2 = df_class2.iloc[:, 1:]
 
-    #convert to 1d array
-    return df_class1.to_numpy().flatten(), df_class2.to_numpy().flatten()
+    df_class1 = df_class1.apply(np.sort, axis=0)
+    df_class2 = df_class2.apply(np.sort, axis=0)
+
+    return df_class1, df_class2
 
 #compute distance between two points
 def distance(a, b):
     return np.sqrt(((a - b) ** 2))
 
 #finds nearest neighbor in array
-def find_nearest(class1, data):
+def find_nearest(df, feature, data):
+    class1 = df.iloc[:, feature]
     dist = np.array([distance(data, x) for x in class1])
     nearest = np.argmin(dist)
     return nearest
-
 
 def debug_csv():
     class1, class2 = read_data("CS170_Small_Data__10.txt")
@@ -34,7 +35,43 @@ def debug_csv():
     print(class1)
     print("\nClass 2:")
     print(class2)
-    index = (find_nearest(class1, 1.1))
-    print(index, class1[index])
+    sample_data = 2
+    feature = 2
+    index = (find_nearest(class1, feature, sample_data))
+    print(index, class1.iloc[index, feature])
 
 debug_csv()
+
+def feature_search(df):
+    #start empty feature set
+    curr_features = []
+    num_features = df.columns
+
+    #for i = 1 to i <= feature, search the ith level
+    #try combinations of features, select best at each level, move forward to combos of best feature + others
+    for i in range(num_features):
+        print(f"On the {i+1}th level of the search tree")
+        feature_to_add_at_this_level = None
+        best_so_far_accuracy = 0
+        for j in range(num_features):
+            #once we add a feature, we should not add it again
+            if j not in curr_features:
+                print(f"-- Considering adding the {j+1} feature")
+                #data = , need to define this
+                accuracy = leave_one_out_cross_validation(data, curr_features, j)
+
+                #if new accuracy is better than prev accuracy, we can add feature j
+                if accuracy > best_so_far_accuracy:
+                    best_so_far_accuracy = accuracy
+                    feature_to_add = j
+                
+        #add to current features
+        curr_features.append(feature_to_add)
+        print(f"On level {i+1}, I added feature {feature_to_add_at_this_level+1} " f"to current set {curr_features}")
+
+    return curr_features
+
+def leave_one_out_cross_validation(somedata, features, i):
+    print("tbd")
+
+
